3e4b07893e304f5dbd36f1904e935828ee3382e1
jborruel308@me.com <jborruel308@me.com>
2024-08-28 21:31:15 -0700

    number 3 completed

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const collectibles = [
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99}
 ]
 
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
 
 // syntax app. method('/route', (req,res) => {})
 
@@ -32,8 +42,22 @@ app.get('/roll/:number', (req,res) => {
 });
 
 //3.
+app.get('/collectibles/:id', (req, res) => {
+   let id = parseInt(req.params.id, 10);
 
+   if (id >= 0 && id < collectibles.length){
+       let foundCollectible = collectibles[id];
+       res.send(` You chose the ${foundCollectible.name}? For ${foundCollectible.price.toFixed(2)}!`)
+   }else {
+    res.send('Sorry this item in not in stock. Check back soon!')
+   }
+})
 
+//4. 
+
+app.get('/shoes', (req,res) => {
+
+}); 
 
 
 
